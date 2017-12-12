08f996faa32baf8280179b448a6745dec0bf1709
chenIshi <chen.ishi@gmail.com>
2017-12-12 17:47:31 +0800

    add lib and modify relative location

--- js/reveal.js
+++ js/reveal.js
@@ -12,13 +12,13 @@ window.onload = function() {
         transition: 'slide', // none/fade/slide/convex/concave/zoom
     
         dependencies: [
-            { src: '../../../../git-repos/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
-            { src: '../../../../git-repos/reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-            { src: '../../../../git-repos/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-            { src: '../../../../git-repos/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-            { src: '../../../../git-repos/reveal.js/plugin/search/search.js', async: true },
-            { src: '../../../../git-repos/reveal.js/plugin/zoom-js/zoom.js', async: true },
-            { src: '../../../../git-repos/reveal.js/plugin/notes/notes.js', async: true }
+            { src: 'reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+            { src: 'reveal.js/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            { src: 'reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+            { src: 'reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+            { src: 'reveal.js/plugin/search/search.js', async: true },
+            { src: 'reveal.js/plugin/zoom-js/zoom.js', async: true },
+            { src: 'reveal.js/plugin/notes/notes.js', async: true }
         ]
     });
     
